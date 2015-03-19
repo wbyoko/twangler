33bca93141005541f7c4590cbfddd33b85be3bd7
William Bagayoko <wbagayoko@gmail.com>
2015-03-18 21:50:59 -0500

    update index.php

--- www-src/php/index.php
+++ www-src/php/index.php
@@ -1,35 +1,6 @@
 <?php
 	date_default_timezone_set('America/Tegucigalpa');
 
-	//see http://stackoverflow.com/questions/4587425/php-function-that-receive-a-cron-string-and-return-the-next-run-timestamp
-	function parse_crontab($crontab) {
-		$time = 'now';
-		$time=explode(' ', date('i G j n w', strtotime($time)));
-		$crontab=explode(' ', $crontab);
-		foreach ($crontab as $k=>&$v) {
-			$v=explode(',', $v);
-			foreach ($v as &$v1) {
-				$v1=preg_replace(
-					array('/^\*$/', '/^\d+$/', '/^(\d+)\-(\d+)$/', '/^\*\/(\d+)$/'),
-					array('true', $time[$k].'===\0', '(\1<='.$time[$k].' and '.$time[$k].'<=\2)', $time[$k].'%\1===0'),
-					$v1
-				);
-			}
-			$v='('.implode(' or ', $v).')';
-		}
-		$crontab=implode(' and ', $crontab);
-		return eval('return '.$crontab.';');
-	}
-
-	function getRandomTrendingTopic() {
-		$url = "http://api.whatthetrend.com/api/v2/trends.json?api_key=96656a87ec6ae60b665194ecb5cd4134420bbd02&place_type_code=2443945";
-		$json = file_get_contents($url);
-		$trends = json_decode($json, true);
-		$trends = $trends["trends"];
-		$randomTrend = $trends[array_rand($trends)];
-		return $randomTrend["name"];
-	}
-
 	$cssFilepath = "www/css/main.c.css";
 	$cssFile = fopen($cssFilepath, 'r');
 	$css = fread($cssFile, filesize($cssFilepath));
@@ -40,32 +11,12 @@
 	$js = fread($jsFile, filesize($jsFilepath));
 	fclose($jsFile);
 
-	$codebirdFilepath = "www/js/codebird.js";
+	$codebirdFilepath = "www/js/codebird.min.js";
 	$codebirdFile = fopen($codebirdFilepath, 'r');
 	$codebird = fread($codebirdFile, filesize($codebirdFilepath));
 	fclose($codebirdFile);
 
-
-	//see http://en.wikipedia.org/wiki/Cron
-	if (parse_crontab('* 10-19 * * 0')) //Sunday Football
-		$defaultQuery = '#nfl';
-	elseif (parse_crontab('* 21 * * 0')) //Homeland / Boardwalk Empire tv show
-		$defaultQuery = '#Homeland OR #BoardwalkEmpire';
-	elseif (parse_crontab('* 21 * * 1')) //Revolution tv show
-		$defaultQuery = '#Revolution';
-	elseif (parse_crontab('* 20-23 * * 0')) //BCS standings release
-		$defaultQuery = '#bcs';
-	elseif (parse_crontab('* 12-19 * * 0')) //Sunday Football
-		$defaultQuery = '#nfl';
-	elseif (parse_crontab('* 18-22 * * 1')) //Monday Night Football
-		$defaultQuery = '#nfl';
-	elseif (parse_crontab('* 9 * * 1-5')) //ESPN First Take
-		$defaultQuery = '#ESPNFirstTake';
-	elseif (parse_crontab('* 11-22 * * 6')) //NCAA Football
-		$defaultQuery = '#football';
-	else
-		//$defaultQuery = 'meme';
-		getRandomTrendingTopic();
+	$defaultQuery = '#trending';
 
 ?><!DOCTYPE html>
 <html>
